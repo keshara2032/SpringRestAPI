31c66c9dfd4418f76728e5b0d3acbc41973c70ee
Keshara Weerasinghe <keshara2032@gmail.com>
2021-02-15 11:05:55 +0530

    added Swagger UI and API Documentation

--- src/main/java/com/country/countryrestapi/CountryRestApiApplication.java
+++ src/main/java/com/country/countryrestapi/CountryRestApiApplication.java
@@ -2,8 +2,12 @@ package com.country.countryrestapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class CountryRestApiApplication {
 
 	public static void main(String[] args) {
